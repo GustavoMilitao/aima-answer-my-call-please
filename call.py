4e0d723b6fdde419de815308d1a21eef62d9d2c6
Gustavo Militão <gustavo.h.militao@gmail.com>
2025-04-18 16:27:38 +0100

    fix address

--- call.py
+++ call.py
@@ -4,7 +4,7 @@ client = Client('TWILIO_SID', 'TWILIO_TOKEN')
 
 # Liga pra AIMA
 call = client.calls.create(
-    to="+351NUMERO_DA_AIMA",
-    from_="+NÚMERO_TWILIO",
-    url="https://github.com/GustavoMilitao/aima-answer-my-call-please/espera-e-redireciona.xml"
+    to="+351217115000",
+    from_="+16368371829",
+    url="https://raw.githubusercontent.com/GustavoMilitao/aima-answer-my-call-please/refs/heads/main/espera-e-redireciona.xml"
 )
